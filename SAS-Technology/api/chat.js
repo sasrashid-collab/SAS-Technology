7335daa6d365bdd2bd1dbf3fe5bce6ed8ed9d920
ئابى كوردى بو كورد <sas.rashid@gmail.com>
2026-06-30 02:03:15 +0300

    Delete SAS-Technology directory

--- SAS-Technology/api/chat.js
+++
@@ -1,6 +0,0 @@
-// ئەم ناونیشانە نوێیە بخەرە ناو فایلی index.html لە بەشی fetch
-const response = await fetch("/api/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: text })
-});
